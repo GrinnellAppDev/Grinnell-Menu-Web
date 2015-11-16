a61b31215e289e22a1c72a89e48538b7c00e2329
grinnellappdev <appdev@grinnell.edu>
2015-11-16 02:14:26 +0000

    Remove blank lines & rails-generator comments

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,45 +1,43 @@
 require "http"
 
 class MenusController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  
+
   def menu
-    
+
     @date = Date.new params[:year].to_i,  params[:month].to_i, params[:day].to_i
-    
+
     menu_response = retrieve_menu @date
-    
+
     handle_menu_response menu_response
-    
+
   end
-  
+
   private
   
   def retrieve_menu(date)
     menu_url = menu_url_for_date date
     return HTTP.get(menu_url)
   end
-  
+
   def handle_menu_response(menu_response)
     if menu_response.status_code.between?(200, 299)
       @menu = JSON.parse menu_response.body
-      
+
       prepare_menu @menu
     else
       not_found
     end
   end
-  
+
   def menu_url_for_date(date)
     "http://appdev.grinnell.edu/glicious/#{date.month}-#{date.day}-#{date.year}.json"
   end
-  
+
   def prepare_menu(menu)
     menu.delete("PASSOVER")
   end
-  
+
   def not_found
     raise ActionController::RoutingError.new('Not Found')
   end
